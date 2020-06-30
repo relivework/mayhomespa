1a033127cd3eb6e9568f51aa20b72122d86f7ee8
rizwan <chouhan>
2020-06-30 14:28:46 +0530

    wow update

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,12 @@
-const { src, dest } = require("gulp");
+const { src, dest, series } = require("gulp");
 const babel = require("gulp-babel");
 var concat = require("gulp-concat");
-exports.default = function () {
-  return src("assets/js/*.js").pipe(concat("root.js")).pipe(dest("dist"));
+const jsBundling = function () {
+  return src("assets/js/*.js").pipe(concat("root.js")).pipe(dest("assets/dist"));
 };
+
+const cssBundling = function () {
+  return src("assets/css/*.css").pipe(concat("root.css")).pipe(dest("assets/dist"));
+};
+
+exports.default = series(jsBundling, cssBundling);
